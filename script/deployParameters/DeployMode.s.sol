b192923826933adbbbd29cc9942a74ad5f8edd60
darshitbhalodi5 <darshitbhalodi@gmail.com>
2024-08-23 09:27:01 +0530

    New contract

---
+++ script/deployParameters/DeployMode.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployMode is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0x4200000000000000000000000000000000000006,
+            seaportV1_5: 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC,
+            seaportV1_4: 0x00000000000001ad428e4906aE43D8F9852d0dD6,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x5091730383fE325040813281231D323049Eeaf8b,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0xAb8509Dd8d8D3b53436AEd524e4a1986BA40bA77;
+    }
+}
